4edd33ef902330938b7aa07caaa8f1b450631ad7
mardecera <18353569+mardecera@users.noreply.github.com>
2026-05-01 11:46:31 -0500

    fix: load styles

--- src/layouts/DocsLayout/DocsLayout.tsx
+++ src/layouts/DocsLayout/DocsLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom"
 import { components } from "./DocsLayout.const"
-import { Sidebar } from "@banji-ui"
+import { Sidebar } from "banji-ui"
 
 const DocsLayout = () => {
 	const { pathname } = useLocation()
